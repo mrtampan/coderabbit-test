e09e52b9387eb3fc730ef9e426256347e84d3e34
Achmad Rivaldi <mrtampan54@gmail.com>
2025-07-24 14:31:14 +0700

    fix index.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 $rabbit = 'harimau';
 $cat = 'kucing';
 $dog = 'anjing';
+$duck = 'bebek';
 
 function getAnimal($rabbit = 'rabbit', $cat = 'cat', $dog = 'dog') {
     return $rabbit . ', ' . $cat . ', ' . $dog;
 }
 
-echo getAnimal($rabbit, $cat, $dog, 1231);
+echo getAnimal($rabbit, $cat, $dog);
 
 ?>
